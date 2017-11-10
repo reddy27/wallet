62ec5ef834b99922229e3ab951fb27999c8a6fe1
reddy27 <gaddamdev5@gmail.com>
2017-11-09 18:15:58 -0800

    tes

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # dummy
 # test
-# test
-# wallet
--- app.js
+++ app.js
@@ -9,10 +9,12 @@ var bodyParser = require('body-parser');
 app.use(bodyParser());
 app.use(bodyParser.urlencoded());
 const pug = require('pug');
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
 app.use(express.static('pass'));
-app.use(express.static('views'));
+app.use(express.static('static'));
 app.use(express.static('images'));
+const url = require('url');
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -92,7 +94,7 @@ var template = passbook.createTemplate('generic', {
 
 // pass.pipe(fs.createWriteStream('./pass/eci.pkpass'));
 
-app.use(express.static('images'));
+//app.use(express.static('images'));
 //app.use('/static', express.static(path.join(__dirname, 'images')));
 
 app.get('/passhtml', function(req, res){
@@ -152,36 +154,52 @@ app.post('/create-pass', function(req, res){
       logo2x: __dirname + '/images/logo@2x.png'
     }
       //console.log('passFields', passFields);
+      function uuidv4() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+          var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+          return v.toString(16);
+        });
+    }
 
       var pass = template.createPass(passFields);
+      let storeId = uuidv4();
       res.setHeader('content-type', 'application/vnd.apple.pkpass');
-      pass.pipe(fs.createWriteStream('./pass/' + date + '.pkpass'));
-       pass.pipe(fs.createWriteStream('./views/' + date + '.pkpass'));
-      res.send(date);
-     res.end();
+      pass.pipe(fs.createWriteStream('./pass/' + storeId + '.pkpass'));
+      pass.pipe(fs.createWriteStream('./views/' + storeId + '.pkpass'));
+      res.send(storeId);
+      res.end();
 });
 
 app.get('/', function(req, res){
   res.send('Hi, there am node service to generate a pass. routes are /passhtml{get}, /get-created-pass/{get}, /givemepass{post}, /givemepass{get} ');
 });
 app.get('/get-created-pass/', cors(), function (req, res) {
-  const orderID = req.query.id;
-  console.log('query', orderID);
+  //const orderID = req.query.id;
+  console.log('query', req);
   res.setHeader('content-type', 'application/vnd.apple.pkpass');
   res.sendFile( orderID + '.pkpass', { root: path.join(__dirname, './pass') });
   res.end();
 
 });
 
-app.get('/custom-page/', function (req, res) {
-  const orderID = req.query.id;
-    //../pass/16:18:35.pkpass
-   dirForPass = "../views/" + orderID + ".pkpass";
-  // console.log('dirForPass', dirForPass);
-  console.log('dir name', __dirname );
- 
-  res.render('index', { title: 'Hey', message: dirForPass });
+app.get('/redirected-page', function(req, res){
+    const dirForPass = req.query.a;
+    console.log('dirForPass', dirForPass);
+    res.render('index', { title: 'Hey', message: dirForPass });
 });
+
+app.get('/custom-page/', function (req, res) {
+   const orderID = req.query.id;
+   //res.redirect('/redirected-page/' +  orderID);
+    res.redirect(url.format({
+       pathname:"/redirected-page",
+       query: {
+          "a": orderID,
+        }
+     }));
+
+});
+
 
 app.get('/givemepass', cors(), function (req, res) {
   console.log('pass location', __dirname);
