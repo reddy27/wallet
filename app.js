a43a0110a903715251a39ad2095221bdf132bcc8
reddy27 <gaddamdev5@gmail.com>
2017-11-20 14:39:32 -0800

    test

--- app.js
+++ app.js
@@ -14,310 +14,194 @@ app.use(express.static('pass'));
 app.use(express.static('static'));
 app.use(express.static('images'));
 const url = require('url');
-
 const testFolder = './pass/';
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
+app.set('port', process.env.PORT || 3000);
 
-app.set('port', process.env.PORT || 3000 );
 
-var passFields = {
-      serialNumber:        'E5982H-I2',
-      barcode: {
-        message:         '123456789',
-        format:          'PKBarcodeFormatQR',
-        messageEncoding: 'iso-8859-1'
-      },
-      organizationName: 'ECI',
-      logoText:         'ECI',
-      description:      'ECI Appt confirmation',
-      foregroundColor:  'rgb(22, 55, 110)',
-      backgroundColor:  'rgb(13, 93, 141)',
-     generic : {
-    primaryFields : [
-      {
-        key : 'name',
-        label : "NAME",
-        value : "Pramod"
-      }
-    ],
-    secondaryFields : [
-      {
-        key : "title",
-        label : "TITLE",
-        value : "A Cool Product Owner"
-      }
-    ],
-    auxiliaryFields : [
-      {
-        key : "email",
-        label : "EMAIL",
-        value : "JAD@test.com"
-      },
-      {
-        key : "twitter",
-        label : "Appointment",
-        value : "10:40Pm"
-      }
-    ],
-    backFields : [
-      {
-        label : "NAME",
-        key : "nameBack",
-        value : "kp.org"
-      }
-    ]
-  },
-      icon:   __dirname + '/images/icon.png',
-      icon2x: __dirname + '/images/icon@2x.png',
-      logo:   __dirname + '/images/logo.png',
-      logo2x: __dirname + '/images/logo@2x.png'
-    }
-
-var template = passbook.createTemplate('generic', {
-  passTypeIdentifier: 'pass.org.kp.eciwalletpoc',
-  teamIdentifier:     'NP763NDP24',
-  organizationName:   'kp.org', 
-}, {
-  certs: {
-    wwdr: './certificate/wwdr.pem',
-    pass: './certificate/CertificateName.pem', // pem with certificate and private key
-    password: 'cadillaC5!' // pass phrase for the pass_cert.pem file
-  }
+app.get('/', function (req, res) {
+    res.send('Hi, there am node service to generate a pass. routes are /new-pass-creation, /get-created-pass/, /custom-page/ ..../passhtml{get}, /get-created-pass/{get}, /givemepass{post}, /givemepass{get} ');
 });
-
-
-
-// var pass = template.createPass(passFields);
-app.get('/new-pass-creation', function(req, res){
-
-  var passFields = {
-      serialNumber:        'E5982H-I2',
-      barcode: {
-        message:         '123456789',
-        format:          'PKBarcodeFormatQR',
-        messageEncoding: 'iso-8859-1'
-      },
-      organizationName: 'ECI',
-      logoText:         'ECI',
-      description:      'ECI Appt confirmation',
-      foregroundColor:  'rgb(22, 55, 110)',
-      backgroundColor:  'rgb(13, 93, 141)',
-     generic : {
-    primaryFields : [
-      {
-        key : 'name',
-        label : "NAME",
-        value : "Pramod"
-      }
-    ],
-    secondaryFields : [
-      {
-        key : "title",
-        label : "TITLE",
-        value : "A Cool Product Owner"
-      }
-    ],
-    auxiliaryFields : [
-      {
-        key : "email",
-        label : "EMAIL",
-        value : "JAD@test.com"
-      },
-      {
-        key : "twitter",
-        label : "Appointment",
-        value : "10:40Pm"
-      }
-    ],
-    backFields : [
-      {
-        label : "NAME",
-        key : "nameBack",
-        value : "kp.org"
-      }
-    ]
-  },
-      icon:   __dirname + '/images/icon.png',
-      icon2x: __dirname + '/images/icon@2x.png',
-      logo:   __dirname + '/images/logo.png',
-      logo2x: __dirname + '/images/logo@2x.png'
-    }
-
-var template = passbook.createTemplate('generic', {
-  passTypeIdentifier: 'pass.org.kp.eciwalletpoc',
-  teamIdentifier:     'NP763NDP24',
-  organizationName:   'kp.org', 
-}, {
-  certs: {
-    wwdr: './certificate/wwdr.pem',
-    pass: './certificate/CertificateName.pem', // pem with certificate and private key
-    password: 'cadillaC5!' // pass phrase for the pass_cert.pem file
-  }
-});
-  var pass = template.createPass(passFields);
-  pass.pipe(fs.createWriteStream('./pass/testpass.pkpass'));
-  console.log('pass created with new login at /pass');
-  res.send('done creating pass');
-  res.end();
-});
-
 // pass.pipe(fs.createWriteStream('./pass/eci.pkpass'));
-
 //app.use(express.static('images'));
 //app.use('/static', express.static(path.join(__dirname, 'images')));
+app.post('/create-pass', function (req, res) {
+    var requestBody = req.body.passDetails;
+    var firstName = requestBody.leadingfields[0].firstName;
+    var aptTime = requestBody.leadingfields[0].appointmentTime;
+    var aptDate = requestBody.leadingfields[0].appointmentDate;;
+    var provider = requestBody.leadingfields[0].providerName;;
+    var dept = requestBody.leadingfields[0].locationInstructions;
+    var getOtherFields = requestBody.backfields[0];
+    var getAddr = requestBody.backfields[0].address1
+        , state = getAddr.State
+        , strAddr = getAddr.StreetAddress
+        , postal = getAddr.PostalCode;
+    const Addr = strAddr + state.Abbreviation + postal;
+    const amount = getOtherFields.amount;
+    const status = getOtherFields.paymentStatus;
+    const message = getOtherFields.message;
+    const contact = getOtherFields.contactReschedule;
+    console.log('fields', requestBody, firstName, aptTime, aptDate ,provider, dept, getOtherFields);
 
-app.get('/passhtml', function(req, res){
-  console.log('passhtml', __dirname + '/images/button.html');
-  res.sendFile(__dirname + '/images/button.html');
+    var template = passbook.createTemplate('generic', {
+          passTypeIdentifier: 'pass.org.kp.eciwalletpoc',
+          teamIdentifier:     'NP763NDP24',
+          organizationName:   'kp.org', 
+        }, {
+          certs: {
+            wwdr: './certificate/wwdr.pem',
+            pass: './certificate/CertificateName.pem', // pem with certificate and private key
+            password: 'cadillaC5!' // pass phrase for the pass_cert.pem file
+  }
 });
-
-app.post('/create-pass', function(req, res){
-  var aptTime = req.body.appointmenttime;
-  var aptDate = req.body.appointmentdate;
-  var provider = req.body.provider;
+    // console.log('req', req.body.passDetails);
+    // var firstName = req.body.firstname;
+    // var aptTime = req.body.appointmenttime;
+    // var aptDate = req.body.appointmentdate;
+    // var provider = req.body.provider;
+    // var dept = req.body.department;
+    // var addr = req.body.address;
     function uuidv4() {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-          var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-          return v.toString(16);
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0
+                , v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
         });
     };
-//var appointmentTime = req.body.appointmentTime;
-var d = new Date();
-  var passFields = {
-      serialNumber:        uuidv4(),
-      barcode: {
-        message:         '123456789',
-        format:          'PKBarcodeFormatQR',
-        messageEncoding: 'iso-8859-1'
-      },
-      organizationName: 'ECI',
-      logoText:         'KAISER PERMEANTE',
-      description:      "ECI confirmation",
-      foregroundColor:  'rgb(22, 55, 110)',
-      backgroundColor:  'rgb(13, 93, 141)',
-     generic : {
-    secondaryFields : [
-      {
-        key : "subtitle",
-        label : "Appointment Time",
-        value : aptTime
-      },
-      {
-        key : "subtitle",
-        label : "Appointment Date",
-        value : aptDate
-      }
-    ],
-    auxiliaryFields : [
-      {
-        key : "subtitle",
-        label : "Provider",
-        value : provider
-      },
-      {
-        key : "Department/Location",
-        label : "Department/Location",
-        value : "1st Floor, Anhamin karmer med office 1"
-      },
-       {
-        key : "Address",
-        label : "Address",
-        value : "3460 E LA PLAMA AVE, ANHEMIN CA 92806-00000"
-      },
-       {
-        key : "Est Cost Share",
-        label : "Est Cost Share",
-        value : "$20"
-      }
-    ],
-    backFields : [
-      {
-        label : "NAME",
-        key : "nameBack",
-        value : "kp.org"
+    //var appointmentTime = req.body.appointmentTime;
+    var d = new Date();
+    var passFields = {
+            serialNumber: uuidv4()
+            , barcode: {
+                message: '123456789'
+                , format: 'PKBarcodeFormatQR'
+                , messageEncoding: 'iso-8859-1'
+            }
+            , organizationName: 'ECI'
+            , description: "Express Check In QR code"
+            , foregroundColor: 'rgb(255, 255, 255)'
+            , backgroundColor: 'rgb(0, 106, 163)'
+            , labelColor: 'rgb(255, 255, 255)'
+            , "generic": {
+                "primaryFields": [
+                    {
+                        "key": "depart"
+                        , "label": "APPOITMENT IS FOR"
+                        , "value": firstName
       }
     ]
-  },
-      icon:   __dirname + '/images/icon.png',
-      icon2x: __dirname + '/images/icon@2x.png',
-      logo:   __dirname + '/images/logo.png',
-      logo2x: __dirname + '/images/logo@2x.png'
-    }
-      //console.log('passFields', passFields);
-      function uuidv4() {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-          var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-          return v.toString(16);
-        });
-    }
+                , "auxiliaryFields": [
 
-      var pass = template.createPass(passFields);
-      let storeId = uuidv4();
-      res.setHeader('content-type', 'application/vnd.apple.pkpass');
-      pass.pipe(fs.createWriteStream('./pass/' + storeId + '.pkpass'));
-      pass.pipe(fs.createWriteStream('./views/' + storeId + '.pkpass'));
-      res.send(storeId);
-      res.end();
-});
-
-app.get('/', function(req, res){
-  res.send('Hi, there am node service to generate a pass. routes are /passhtml{get}, /get-created-pass/{get}, /givemepass{post}, /givemepass{get} ');
-});
-app.get('/get-created-pass/', cors(), function (req, res) {
-  const orderID = req.query.id;
-  res.setHeader('content-type', 'application/vnd.apple.pkpass');
-  res.sendFile( orderID + '.pkpass', { root: path.join(__dirname, './pass') });
-  fs.readdir(testFolder, (err, files) => {
-      files.forEach(file => {
-        console.log('get-created-pass', file);
-      });
-    });
-
-  res.end();
-
-});
-
-app.get('/redirected-page', function(req, res){
-    //const dirForPass = req.query.a + ".pkpass";
-    const dirForPass =  "testpass.pkpass";
-    console.log('query is :', dirForPass);
-
-    fs.readdir(testFolder, (err, files) => {
-      files.forEach(file => {
-        console.log('list of files under pass', file);
-         console.log('dirname', __dirname);
-      });
-    });
-
-    //res.setHeader('content-type', 'application/vnd.apple.pkpass');
-    res.render('index', { title: 'Hey', message: dirForPass });
-});
-
-app.get('/custom-page/', function (req, res) {
-   const orderID = req.query.id;
-   //res.redirect('/redirected-page/' +  orderID);
-     //res.setHeader('content-type', 'application/vnd.apple.pkpass');
-     res.redirect(url.format({
-       pathname:"/redirected-page",
-       query: {
-          "a": orderID,
+                    {
+                        "label": "DEPARTMNENT/LOCATION"
+                        , "key": "depatmentNewName"
+                        , "value": dept
+      }
+    ]
+                , "backFields": [
+                    {
+                        "key": "passport"
+                        , "label": "APPOITMENTS IS FOR"
+                        , "value": firstName
+      }
+                    , {
+                        "label": "DATE"
+                        , "key": "flightNewName"
+                        , "value": aptDate
+      }
+                    , {
+                        "label": "TIME"
+                        , "key": "flightNewName"
+                        , "value": aptTime + "\r\n\r\nPlease arrive 15min earley"
+      , }
+                    , {
+                        "label": "PROVIDER NAME"
+                        , "key": "flightNewName"
+                        , "value": provider
+      }
+                    , {
+                        "label": "DEPARTMNENT/LOCATION"
+                        , "key": "flightNewName"
+                        , "value": dept
+      }
+                    , {
+                        "label": "ADDRESS"
+                        , "key": "flightNewName"
+                        , "value": Addr
+      }
+                    , {
+                        "label": "EST COST SHARE"
+                        , "key": "flightNewName"
+                        , "value": amount
+      }
+                    , {
+                        "label": "PAYMENT STATUS"
+                        , "key": "flightNewName"
+                        , "value": status
+      }
+                    , {
+                        "label": "CONTACT/RESCHEDULE"
+                        , "key": "flightNewName"
+                        , "value": contact
+      }
+                    , {
+                        "label": "MESSAGE"
+                        , "key": "flightNewName"
+                        , "value": message
+      , }
+    ]
+            }
+            , icon: __dirname + '/images/icon.png'
+            , icon2x: __dirname + '/images/icon@2x.png'
+            , logo: __dirname + '/images/logo.png'
+            , logo2x: __dirname + '/images/logo@2x.png'
         }
-     }));
-
+        //console.log('passFields', passFields);
+   
+    var pass = template.createPass(passFields);
+    let storeId = uuidv4();
+    res.setHeader('content-type', 'application/vnd.apple.pkpass');
+    //pass.pipe(fs.createWriteStream('./pass/' + storeId + '.pkpass'));
+    pass.pipe(fs.createWriteStream('./views/' + storeId + '.pkpass'));
+    res.send(storeId);
+    res.end();
 });
 
 
-app.get('/givemepass', cors(), function (req, res) {
-  console.log('pass location', __dirname);
-  res.setHeader('content-type', 'application/vnd.apple.pkpass');
-  res.sendFile('pass.pkpass', { root: path.join(__dirname, './pass') });
-  res.end();
+
+app.get('/redirected-page', function (req, res) {
+    const dirForPass = req.query.passId + ".pkpass";
+    //const dirForPass =  "testpass.pkpass";
+    console.log('query is :', dirForPass);
+    fs.readdir(testFolder, (err, files) => {
+        files.forEach(file => {
+            console.log('list of files under pass', file);
+            console.log('dirname', __dirname);
+        });
+    });
+    //res.setHeader('content-type', 'application/vnd.apple.pkpass');
+    res.render('index', {
+        title: 'Hey'
+        , message: dirForPass
+    });
 });
-var server = app.listen(app.get('port'), function() {
-  //console.log('Listening on port ' + app.get('port'));
+app.get('/custom-page/', function (req, res) {
+    const orderID = req.query.id;
+    //res.redirect('/redirected-page/' +  orderID);
+    //res.setHeader('content-type', 'application/vnd.apple.pkpass');
+    res.redirect(url.format({
+        pathname: "/redirected-page"
+        , query: {
+            "passId": orderID
+        , }
+    }));
+});
+
+var server = app.listen(app.get('port'), function () {
+    console.log('Listening on port ' + app.get('port'));
 });
